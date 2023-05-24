0b0b6fb36cc397aab3ae07c18628da4ac7840ac3
jLereback <112405931+jLereback@users.noreply.github.com>
2023-05-24 11:14:29 +0200

    Add implementations of StringCalculator.java

---
+++ TextProvider/src/main/java/se/iths/provider/Sentence.java
@@ -0,0 +1,10 @@
+package se.iths.provider;
+
+import se.iths.service.StringCalculator;
+
+public class Sentence implements StringCalculator {
+    @Override
+    public int calculate(String stringToCalculate) {
+        return 0;
+    }
+}
