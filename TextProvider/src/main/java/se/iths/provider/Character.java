3acd67c85da5b4acced29f3e194d054c2b046033
jLereback <112405931+jLereback@users.noreply.github.com>
2023-05-25 22:44:56 +0200

    Update Enum från LETTER tå CHARACTER

--- TextProvider/src/main/java/se/iths/provider/Character.java
+++ TextProvider/src/main/java/se/iths/provider/Character.java
@@ -1,11 +1,11 @@
 package se.iths.provider;
 
 import se.iths.service.StringCalculator;
-import se.iths.service.Type;
-import se.iths.service.Calculate;
+import se.iths.service.annotation.Type;
+import se.iths.service.annotation.Calculate;
 
-@Calculate(Type.LETTER)
-public class Letter implements StringCalculator {
+@Calculate(Type.CHARACTER)
+public class Character implements StringCalculator {
     @Override
     public int calculate(String stringToCalculate) {
         return stringToCalculate.length();
